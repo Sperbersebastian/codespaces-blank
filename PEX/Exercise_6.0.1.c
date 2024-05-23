963513558b8c5bdf4b2cc88da24e3c8ac16bf031
Sperbersebastian <86466597+Sperbersebastian@users.noreply.github.com>
2024-05-23 08:30:17 +0000

    new file:   PEX/Ex6 	modified:   PEX/Exercise_6.0.1.c 	modified:   PEX/bashpipe.text 	new file:   PEX/output.txt

--- PEX/Exercise_6.0.1.c
+++ PEX/Exercise_6.0.1.c
@@ -11,11 +11,15 @@ double cube(double num) {
 }
 
 double squareRoot(double num) {
+    if (num < 0) {
+        printf("Error: Negative input for square root.\n");
+        return -1;
+    }
     return sqrt(num);
 }
 
 // 2. Use those functions to calculate square and square root for integers from 0 to 1000 and write the output into a file.
-int main() {
+int task2() {
     double number;
     printf("Enter a number: ");
     scanf("%lf", &number);
@@ -49,5 +53,112 @@ int main() {
 
 // 3. Modify the program to take user input numbers until a given stop condition. Write the numbers, squares, cubes and square roots to a user defined file.
 
+#include <stdio.h>
+#include <math.h>
+
+
+int task3() {
+    double number;
+    char filename[100];
+    printf("Enter the output filename: ");
+    scanf("%s", filename);
+
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    printf("Enter numbers to process (enter -1 to stop):\n");
+    while (1) {
+        printf("Enter a number: ");
+        if (scanf("%lf", &number) != 1) {
+            printf("Invalid input.\n");
+            break;
+        }
+
+        if (number == -1) {
+            break;
+        }
+
+        double numSquare = square(number);
+        double numCube = cube(number);
+        double numSquareRoot = squareRoot(number);
+
+        if (numSquareRoot != -1) {
+            fprintf(file, "Number: %.2lf, Square: %.2lf, Cube: %.2lf, Square Root: %.2lf\n", number, numSquare, numCube, numSquareRoot);
+        } else {
+            fprintf(file, "Number: %.2lf, Square: %.2lf, Cube: %.2lf, Square Root: undefined\n", number, numSquare, numCube);
+        }
+    }
+
+    fclose(file);
+    printf("Results written to %s\n", filename);
+
+    return 0;
+}
 
 // 4. Modify the program to ask the user whether to create/overwrite or append the given file.
+
+int task4() {
+    double number;
+    char filename[100];
+    char mode;
+    
+    printf("Enter the output filename: ");
+    scanf("%s", filename);
+
+    printf("Do you want to (o)verwrite or (a)ppend to the file? ");
+    scanf(" %c", &mode);
+
+    FILE *file;
+    if (mode == 'o' || mode == 'O') {
+        file = fopen(filename, "w");
+    } else if (mode == 'a' || mode == 'A') {
+        file = fopen(filename, "a");
+    } else {
+        printf("Invalid mode.\n");
+        return 1;
+    }
+
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    printf("Enter numbers to process (enter -1 to stop):\n");
+    while (1) {
+        printf("Enter a number: ");
+        if (scanf("%lf", &number) != 1) {
+            printf("Invalid input.\n");
+            break;
+        }
+
+        if (number == -1) {
+            break;
+        }
+
+        double numSquare = square(number);
+        double numCube = cube(number);
+        double numSquareRoot = squareRoot(number);
+
+        if (numSquareRoot != -1) {
+            fprintf(file, "Number: %.2lf, Square: %.2lf, Cube: %.2lf, Square Root: %.2lf\n", number, numSquare, numCube, numSquareRoot);
+        } else {
+            fprintf(file, "Number: %.2lf, Square: %.2lf, Cube: %.2lf, Square Root: undefined\n", number, numSquare, numCube);
+        }
+    }
+
+    fclose(file);
+    printf("Results written to %s\n", filename);
+
+    return 0;
+}
+
+
+
+int main() {
+
+    task3();    
+    return 0;
+}
