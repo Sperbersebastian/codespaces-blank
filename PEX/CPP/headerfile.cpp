128655c926af9b042c5fb2d16349765748db674b
Sperbersebastian <86466597+Sperbersebastian@users.noreply.github.com>
2024-06-13 09:13:09 +0000

    modified:   PEX/CPP/headerfile.cpp

--- PEX/CPP/headerfile.cpp
+++ PEX/CPP/headerfile.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
-#include <version>
+#include <string>
 
-// Check for older compilers
-#ifdef __cpp_lib_filesystem
-    #include <filesystem>
-    namespace fs = std::filesystem;
-#elif __cpp_lib_experimental_filesystem
-    #include <experimental/filesystem>
-    namespace fs = std::experimental::filesystem;
-#else
-    #error "no filesystem support ='("
-#endif
+// Forward declaration of pex namespace
+namespace pex {
+    std::string get_home_directory();
+}
+
+class PexBan {
+public:
+    void itemSave() {
+        std::cout << "itemSave method called." << std::endl;
+        std::cout << "Home Directory: " << pex::get_home_directory() << std::endl;
+    }
+    
+    void interactiveMenu() {
+        char choice;
+        while (true) {
+            std::cout << "(s)ave: Save item" << std::endl;
+            std::cout << "(q)uit: Quit" << std::endl;
+            std::cout << "Choose an option: ";
+            std::cin >> choice;
+            if (choice == 's') {
+                itemSave();
+            } else if (choice == 'q') {
+                break;
+            } else {
+                std::cout << "Invalid option." << std::endl;
+            }
+        }
+    }
+};
 
 int main() {
-    // Print the current working directory
-    std::cout << "current_path:\n" << fs::current_path() <<
-    "\nExists Makefile?: " << fs::exists("../Makefile") <<
-    std::endl;
-
+    PexBan pb;
+    pb.interactiveMenu();
     return 0;
 }
