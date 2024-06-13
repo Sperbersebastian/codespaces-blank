52e5cee23afff952ef3483b3726b6751bfc98059
Sperbersebastian <86466597+Sperbersebastian@users.noreply.github.com>
2024-06-13 08:36:56 +0000

    modified:   PEX/CPP/pex.h

--- PEX/CPP/pex.h
+++ PEX/CPP/pex.h
@@ -3,8 +3,10 @@
 
 #include <string>
 
-namespace pex {
-    std::string get_home_directory();
+int main() {
+        std::cout << "PEX Home: " << pex::get_home_directory()
+        << std::endl;
+    return(0);
 }
 
 #endif // PEX_H
